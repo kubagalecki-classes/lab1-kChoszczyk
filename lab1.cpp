6edfd8eefa3e158f1aa5c88ae829dcaa9542d061
kChoszczyk <krzysiex97@gmail.com>
2020-10-29 16:47:31 +0000

    Zadanie 1

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,32 @@
+#include <cmath>
 #include <iostream>
+
+using namespace std;
+
+struct Wektor2D
+{
+    double x;
+    double y;
+    double norm()
+    {
+        double sum, len;
+        sum = x * x + y * y;
+        len = sqrt(sum);
+        cout << "Norma wynosi " << len << "." << endl;
+        return len;
+    }
+    void print()
+    {
+        cout << "Wspolrzedna x wektora " << x << endl;
+        cout << "Wspolrzedna y wektora " << y << endl;
+    }
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Wektor2D vect;
+    vect.x = 10.783;
+    vect.y = 4.652;
+    vect.norm();
+    vect.print();
 }
